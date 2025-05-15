d563da56443a9ba1ee2eef9e1e904c67d76be216
kruzeyy <pontus.maxime@yahoo.com>
2025-05-15 11:26:17 +0200

    Update App component layout and branding with new logo and title

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,32 +1,21 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
 import './App.css'
-import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
-import iconUrl from 'leaflet/dist/images/marker-icon.png'
-import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
-
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-})
 
 function App() {
   return (
-    <div className="App">
-      <h1>Carte de Lyon</h1>
-      <div id="map">
-        <MapContainer center={[45.75, 4.85]} zoom={12} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
-          <TileLayer
-            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[45.75, 4.85]}>
-            <Popup>Lyon, France</Popup>
-          </Marker>
-        </MapContainer>
+    <div className="app-container">
+      <img
+        src="/images/Logo.png"
+        alt="Logo"
+        className="logo"
+        style={{ width: '150px', height: '150px' }}
+      />
+      <h1 className="site-title">Lyon Alerte 360</h1>
+      <div className="image-container">
+        <img
+          src="/images/carte-lyon.png"
+          alt="Carte des arrondissements de Lyon"
+          className="carte-image"
+        />
       </div>
     </div>
   )
