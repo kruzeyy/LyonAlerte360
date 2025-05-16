3781ec75f91ed3ebdb917acd98f21b3b6d6c3866
Maxime <129726934+kruzeyy@users.noreply.github.com>
2025-05-16 09:38:03 +0200

    Implement CSV data streaming

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -10,34 +10,38 @@ import Legal from "./pages/Legal";
 import Footer from "./components/Footer";
 import { ChatProvider } from "./context/ChatContext";
 import { AlertProvider } from "./context/AlertContext";
+import { DataProvider } from "./context/DataContext";
 
 function App() {
   return (
     <Router>
       <AlertProvider>
         <ChatProvider>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1 relative">
-              <div className="container mx-auto px-4 py-6">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/activities" element={<Activities />} />
-                  <Route path="/safety" element={<Safety />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/legal" element={<Legal />} />
-                </Routes>
-              </div>
-              <div className="fixed bottom-4 right-4 z-50">
-                <ChatIcon />
-                <ChatPanel />
-              </div>
-            </main>
-            <Footer />
-          </div>
+          <DataProvider>
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex-1 relative">
+                <div className="container mx-auto px-4 py-6">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/activities" element={<Activities />} />
+                    <Route path="/safety" element={<Safety />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/legal" element={<Legal />} />
+                  </Routes>
+                </div>
+                <div className="fixed bottom-4 right-4 z-50">
+                  <ChatIcon />
+                  <ChatPanel />
+                </div>
+              </main>
+              <Footer />
+            </div>
+          </DataProvider>
         </ChatProvider>
       </AlertProvider>
     </Router>
   );
 }
+
 export default App;
